4722f5208b4e5dd4a49abe45325968153b7fdfd8
Martin Kitonga <119602263+blacksnowmartin@users.noreply.github.com>
2024-02-01 23:19:15 +0300

    Create basic types.ts

---
+++ basic types.ts
@@ -0,0 +1,8 @@
+// Basic variable types
+let myString: string = "Hello, TypeScript!";
+let myNumber: number = 42;
+let isDone: boolean = false;
+
+// Arrays and tuples
+let myArray: number[] = [1, 2, 3];
+let myTuple: [string, number] = ["TypeScript", 2022];
